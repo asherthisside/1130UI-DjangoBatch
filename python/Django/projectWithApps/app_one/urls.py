0c41e0a085265944925ed8eed3087cc2b7365f63
Asher <haarisharry007@gmail.com>
2022-05-18 11:43:00 +0530

    CRUD operatons from admin panel

--- python/Django/projectWithApps/app_one/urls.py
+++ python/Django/projectWithApps/app_one/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('faculty-data', views.facdata, name='faculty'),
+    path('prem-sir-ka-data', views.premsir, name='prem'),
+    path('delete', views.delete, name='delete'),
+    path('update', views.update, name='update'),
 ]
--- python/Django/projectWithApps/app_one/views.py
+++ python/Django/projectWithApps/app_one/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Student, Faculty
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'index.html')
+    students = Student.objects.all()
+    return render(request, 'index.html', {'all_students' : students})
+
+def facdata(request):
+    faculties = Faculty.objects.all()
+    return render(request, 'faculty.html', {'all_faculty' : faculties})
+
+def premsir(request):
+    fac = Faculty.objects.get(faculty_name = 'Prem sir')
+    return render(request, 'prem.html', {'faculty' : fac})
+
+def update(request):
+    return render(request, 'update.html')
+
+def delete(request):
+    return render(request, 'delete.html')
